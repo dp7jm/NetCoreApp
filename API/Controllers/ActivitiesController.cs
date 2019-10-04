3685a1b9b888846d780276635f8452e9fafd6301
{dp7jm} <{dpjm@chef.net}>
2019-10-04 20:31:56 +0200

    working editing activities dataBase

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -33,5 +33,11 @@ namespace API.Controllers
         {
             return await _mediator.Send(command);
         }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
     }
 }
--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -14,7 +14,7 @@ namespace Application.Activities
             public string Title { get; set; }
             public string Description { get; set; }
             public string Category { get; set; }
-            public DateTime Date { get; set; }
+            public DateTime? Date { get; set; }
             public string City { get; set; }
             public string Venue { get; set; }
 
@@ -30,9 +30,21 @@ namespace Application.Activities
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-                //handler logic
+                var activity = await _context.Activities.FindAsync(request.Id);
+                if (activity == null)
+                {
+                    throw new Exception("Could not find activity");
+                }
+                activity.Title = request.Title ?? activity.Title;
+                activity.Description = request.Description ?? activity.Description;
+                activity.Category = request.Category ?? activity.Category;
+                activity.Date = request.Date ?? activity.Date;
+                activity.City = request.City ?? activity.City;
+                activity.Venue = request.Venue ?? activity.Venue;
+
                 var success = await _context.SaveChangesAsync() > 0;
                 if (success) return Unit.Value;
+                
                 throw new Exception("Problem saving changes");
 
 
